bcb559fca9acb39971e1bc2ef5630e1961c19fef
Kevin Kipkemboi <theptjhub@gmail.com>
2023-08-25 11:26:19 -0700

    navbar and sidebar created

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,8 @@
-"use client"
-
-import { UserButton } from "@clerk/nextjs"
 
 export default function DashboardPage() {
   return (
     <div>
         <p>Dashboard Page (Protected)</p>
-        <UserButton afterSignOutUrl="/" /> {/* Bring you back to the landing page */}
     </div>
   )
 }
