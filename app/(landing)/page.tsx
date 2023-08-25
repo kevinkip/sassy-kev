2dd25bafb3619eb290dea12a3d8b0a245c4ed9b2
Kevin Kipkemboi <theptjhub@gmail.com>
2023-08-24 23:22:31 -0700

    setup folder structure

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+// if the user is logged in or not, this is what they'll see. 
+const LandingPage = () => {
+    return (
+        <div>
+            Landing Page (Unprotected)
+        </div>
+    )
+}
+
+export default LandingPage;
