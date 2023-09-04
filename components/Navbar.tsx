a82ff3dca78a3bf57331eec15ec918aab6ab1ead
Kevin Kipkemboi <theptjhub@gmail.com>
2023-09-04 09:36:42 -0700

    fix package.json

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,14 +9,14 @@ const Navbar = async () => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription();
   
-  return (
-    <div className='flex items-center p-4'>
-        <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
-        <div className="flex w-full justify-end">
-          <UserButton afterSignOutUrl='/' />
-        </div>
+  return ( 
+    <div className="flex items-center p-4">
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  )
+   );
 }
 
 export default Navbar;
